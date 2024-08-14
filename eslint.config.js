107b9bba57f5356adcf64af5e4006de97825abfa
吴凯洋 <1272610480@qq.com>
2024-08-14 15:36:24 +0800

    修改eslint.config.js中TS配置；添加element-PLUS类型说明

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,6 @@ const eslintConfigPrettier = require('eslint-config-prettier')
 /**@type {import('eslint').Linter.Config[]}  */
 module.exports = [
   ...eslint.configs['flat/strongly-recommended'],
-  tsEslint.configs.eslintRecommended,
+  tsEslint.config,
   eslintConfigPrettier //关闭所有与prettier规则冲突的Eslint规则
 ]
