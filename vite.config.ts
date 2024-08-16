0003aa8595d9e7c78b3c2309ebf8e9db4409afce
吴凯洋 <1272610480@qq.com>
2024-08-16 18:01:09 +0800

    一些配置文件的修改补丁

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import postcssEnv from 'postcss-preset-env'
 import { resolve } from 'node:path'
 import { checker } from 'vite-plugin-checker'
@@ -7,32 +7,56 @@ import { viteMockServe } from 'vite-plugin-mock'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@component': resolve(__dirname, './src/components'),
-      '@view': resolve(__dirname, './src/view'),
-      '@router': resolve(__dirname, './src/router')
+import { cwd } from 'node:process'
+const envPrefix = 'DB_'
+export default defineConfig(({ command, mode }) => {
+  const resloveProxy = (mode: string) => {
+    const o = {}
+    const p = `/${mode}`
+    o[p] = {
+      target: loadEnv(mode, cwd(), envPrefix).DB_PROXY_URl,
+      changeOrigin: true,
+      rewrite: (path) => path.replace(p, '')
     }
-  },
-  css: {
-    preprocessorOptions: {},
-    postcss: {
-      plugins: [postcssEnv]
-    }
-  },
-  plugins: [
-    checker({
-      vueTsc: true
-    }),
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    }),
-    viteMockServe()
-  ]
+    return o
+  }
+  return {
+    envPrefix,
+    server: {
+      port: parseInt(loadEnv(mode, cwd(), envPrefix).DB_LOCAL_PORT),
+      proxy: resloveProxy(mode)
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '@component': resolve(__dirname, './src/components'),
+        '@view': resolve(__dirname, './src/view'),
+        '@router': resolve(__dirname, './src/router'),
+        '@assets': resolve(__dirname, './src/assets'),
+        '@styles': resolve(__dirname, './src/styles'),
+        '@utils': resolve(__dirname, './src/utils'),
+        '@http': resolve(__dirname, './src/http'),
+        '@stores': resolve(__dirname, './src/stores')
+      }
+    },
+    css: {
+      preprocessorOptions: {},
+      postcss: {
+        plugins: [postcssEnv]
+      }
+    },
+    plugins: [
+      checker({
+        vueTsc: true
+      }),
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      }),
+      viteMockServe()
+    ]
+  }
 })
