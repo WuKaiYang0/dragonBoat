a8539749ad573b895ae7713f9fc815738fe9fd63
吴凯洋 <1272610480@qq.com>
2024-12-03 20:54:50 +0800

    +add 点击tag跳转route

---
+++ src/components/MainContent.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="content">
+    <Sidebar></Sidebar>
+    <main style="overflow: hidden">
+      <div class="content-wrapper">
+        <div class="tags">
+          <el-icon icon v-if="!otherStore.collapse" @click="otherStore.toggleCollapse()"
+            ><Fold
+          /></el-icon>
+          <el-icon icon v-else @click="otherStore.toggleCollapse()"><Expand /></el-icon>
+          <el-divider direction="vertical" />
+          <el-tag
+            :closable="index === 0 ? false : true"
+            effect="plain"
+            v-for="(tag, index) in dynamicTags"
+            class="default"
+            :class="{ active: tag.active }"
+            @close="handleClose(tag, index)"
+            @click="handleClick(tag)"
+          >
+            <ElIcon>
+              <component :is="tag.meta.icon"></component>
+            </ElIcon>
+            <span>{{ tag.meta.title }}</span>
+          </el-tag>
+        </div>
+        <RouterView></RouterView>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Sidebar from './Sidebar.vue'
+
+import { useOtherStore } from '@/stores/other'
+import { ref, watch } from 'vue'
+import { Meta } from '@/typings/common'
+import router from '@/router'
+const otherStore = useOtherStore()
+type TagSetting = { active: boolean; fullPath: string; meta: Meta }
+const dynamicTags = ref<TagSetting[]>([])
+const handleClick = (tag: TagSetting) => {
+  dynamicTags.value.forEach((item) => (item.active = false))
+  tag.active = true
+  otherStore.setActiveRoute({ fullPath: tag.fullPath, meta: tag.meta })
+  otherStore.setActivePath(tag.fullPath)
+  router.push(tag.fullPath)
+}
+//监听otherStore仓库中Getter数据
+watch(
+  () => otherStore.activePath,
+  () => {
+    //在捕获activePath数据更改之前，此时otherStore.activeRoute已更改
+    const newTag: TagSetting = {
+      active: false,
+      fullPath: otherStore.activeRoute.fullPath,
+      meta: otherStore.activeRoute.meta
+    }
+    //看是否存在，存在就取
+    const currentTag = dynamicTags.value.find((t) => t.fullPath === newTag.fullPath)
+    if (!currentTag) {
+      //说明不存在
+      dynamicTags.value.push(newTag)
+      handleClick(newTag)
+    } else {
+      //存在
+      handleClick(currentTag)
+    }
+  },
+  { immediate: true }
+)
+const handleClose = (tag: TagSetting, index: number) => {
+  //第一个是首页，不能被del
+  if (index > 0) {
+    //查找当前Tag在dynamicTags的索引值
+    const currentTagIndex = dynamicTags.value.findIndex((tag) => tag.active === true)
+    console.log(currentTagIndex)
+    //del Tag
+    dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1)
+    if (currentTagIndex >= index) {
+      handleClick(dynamicTags.value[currentTagIndex - 1])
+    }
+    //若前面条件不满足，只删除了没有选择的标签...
+  }
+}
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex: 1;
+  /* height: calc(100% - var(--main-head-height)); */
+
+  main {
+    background-color: #f0f2f5;
+    width: 100%;
+    padding: 15px;
+    .content-wrapper {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      /* background-color: var(--main-background-color); */
+      .tags {
+        height: var(--main-tags-height);
+        background-color: var(--main-background-color);
+        display: flex;
+        align-items: center;
+        padding-left: 5px;
+        .default {
+          box-sizing: border-box;
+          height: calc(100% - 3px);
+          color: #4060c7;
+          margin-right: 10px;
+          border: 1px solid #4060c7;
+          --el-color-primary: #4060c7;
+        }
+        .active {
+          color: #ffffff !important;
+          background-color: #4060c7 !important;
+          --el-color-primary: #4060c7 !important;
+        }
+      }
+    }
+  }
+}
+</style>
