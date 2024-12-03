0829419a1b731576b68d525094efcc587c88ef68
吴凯洋 <1272610480@qq.com>
2024-12-03 20:47:52 +0800

    +<1160px菜单栏自动收缩

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,87 @@
+<template>
+  <!-- <div> -->
+  <el-menu
+    :default-active="otherStore.activePath"
+    class="el-menu-vertical-demo"
+    :collapse="otherStore.collapse"
+    @open="handleOpen"
+    @close="handleClose"
+    v-for="wrapper in otherStore.menus"
+  >
+    <template v-for="rs in wrapper.routes">
+      <el-menu-item
+        v-if="!rs.children"
+        :index="rs.path"
+        @click="menuItemClickHandler({ fullPath: rs.path, meta: rs.meta }, rs.name)"
+      >
+        <el-icon v-if="rs.meta.icon"><component :is="rs.meta.icon" /></el-icon>
+        <span>{{ rs.name }}</span>
+      </el-menu-item>
+      <template v-else="rs.children">
+        <el-sub-menu :index="rs.path">
+          <template #title>
+            <el-icon v-if="rs.meta.icon"><component :is="rs.meta.icon" /></el-icon>
+            <span>{{ rs.name }}</span>
+          </template>
+          <!-- <template #title>{{ rs.name }}</template> -->
+          <el-menu-item
+            v-for="r in rs.children"
+            :index="rs.path + '/' + r.path"
+            @click="
+              menuItemClickHandler({ fullPath: rs.path + '/' + r.path, meta: r.meta }, r.name)
+            "
+          >
+            <template #title>
+              <el-icon v-if="r.meta.icon"><component :is="r.meta.icon" /></el-icon>
+              <span>{{ r.name }}</span>
+            </template>
+          </el-menu-item>
+        </el-sub-menu>
+      </template>
+    </template>
+  </el-menu>
+  <!-- </div> -->
+</template>
+
+<script lang="ts" setup>
+import { useOtherStore } from '@/stores/other'
+import { ActiveRoute } from '@/typings/common'
+import { onMounted, onUnmounted } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const otherStore = useOtherStore()
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  // console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  // console.log(key, keyPath)
+}
+const menuItemClickHandler = (activeRoute: ActiveRoute, name: string) => {
+  router.push({ path: activeRoute.fullPath })
+  //先setActiveRoute
+  //因为MainContent组件内watch->activePath,需要捕获更新后拿到 meta 数据
+  otherStore.setActiveRoute(activeRoute)
+  otherStore.setActivePath(activeRoute.fullPath)
+}
+const setSideBarAuto = () => {
+  if (document.documentElement.offsetWidth <= 1160) {
+    if (!otherStore.collapse) {
+      otherStore.setCollapse(true)
+    }
+  }
+}
+onMounted(() => {
+  window.addEventListener('resize', setSideBarAuto)
+})
+onUnmounted(() => {
+  window.removeEventListener('resize', setSideBarAuto)
+})
+</script>
+
+<style scoped>
+:deep(.el-menu-vertical-demo:not(.el-menu--collapse)) {
+  flex-basis: 200px;
+  height: 100%;
+}
+</style>
